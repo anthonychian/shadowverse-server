0f056e9e6e42ad055b1d7453a29712e932a1cc92
Anthony C <anthonychian@gmail.com>
2024-01-31 11:24:51 -0800

    fix port

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const cors = require("cors");
 
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://shadowverse-client.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
@@ -27,6 +29,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
+server.listen(5000, () => {
+  console.log(`Server has started on port ${PORT}`);
 });
