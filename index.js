510e9d5dc42f2d8cdcfd2a96fc2e5b214a4411b9
Anthony C <anthonychian@gmail.com>
2024-01-31 10:47:46 -0800

    added rooms

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  // console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
 
   socket.on("send msg", (data) => {
-    socket.broadcast.emit("receive msg", data);
+    socket.to(data.room).emit("receive msg", data);
   });
 });
 
