6391872e485f49ff81cd04d7348ca226284e5edb
diana <diliang4783573@gmail.com>
2024-01-31 21:50:22 -0800

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({ origin: "*"}));
 
 const PORT = process.env.PORT || 5000;
 
